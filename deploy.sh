f50e96a2c838473c8870329e55f5c1317d1a7d91
Kashif Nazir <kashifsnazir@gmail.com>
2023-09-21 11:54:15 -0700

    Update deploy script to update lambda function

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Step 1: Make a temporary copy of the directory
+# Config
+function_name="ChatterBotSkillHandler"
+
+echo "Starting deploy..."
+
+# Make a temporary copy of the directory
 temp_dir="tmp-package"
 cp -r "venv/lib/python3.9/site-packages/." "$temp_dir"
 
-# Check if copying was successful
 if [ $? -ne 0 ]; then
   echo "Error: Failed to copy directory"
   exit 1
 fi
 
-# Step 2: Copy all python files from current directory into the temporary directory
+# Copy all python files from current directory into the temporary directory
 find . -maxdepth 1 -name '*.py' -exec cp {} "$temp_dir/" \;
 
-# Check if copying was successful
 if [ $? -ne 0 ]; then
   echo "Error: Failed to copy Python files"
   exit 1
 fi
 
-# Step 3: Zip up the temporary directory (quietly)
+# Zip up the temporary directory (quietly)
 zip_name="package.zip"
 cd "$temp_dir"
 zip -rq "$zip_name" .
@@ -28,19 +31,23 @@ mv "$zip_name" ..
 cd ..
 rm -rf "$temp_dir"
 
-# Check if zipping was successful
 if [ $? -ne 0 ]; then
   echo "Error: Failed to zip the directory"
   exit 1
 fi
 
-# Step 4: Delete the temporary directory
+# Delete the temporary directory
 rm -rf "$temp_dir"
 
-# Check if deletion was successful
 if [ $? -ne 0 ]; then
   echo "Error: Failed to delete the temporary directory"
   exit 1
 fi
 
 echo "Successfully created $zip_name"
+
+aws lambda update-function-code --function-name "$function_name" --zip-file "fileb://$(pwd)/$zip_name"
+
+rm -rf "$zip_name"
+
+echo "Successfully deployed changes"
